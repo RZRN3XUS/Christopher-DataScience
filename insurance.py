f71cdb742794204fea23f2ede82f61fc1e6145ec
Cribober <viridianrobotics@gmail.com>
2022-01-13 10:02:53 -0600

    no

--- insurance.py
+++ insurance.py
@@ -5,7 +5,7 @@ bmi = 26.2
 num_of_children = 3
 smoker = 0
 
-#Formula
+#formula
 insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
 print("This person's insurance cost is " + str(insurance_cost) + " dollars")
 age += 4
