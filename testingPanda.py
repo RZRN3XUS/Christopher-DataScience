a6e07733f00e6e34fc2adc162d50f85d890c4a1b
Cribober <viridianrobotics@gmail.com>
2022-03-07 09:06:05 -0600

    pppp

--- testingPanda.py
+++ testingPanda.py
@@ -11,7 +11,22 @@ df1 = pd.DataFrame(listBig, columns = ['name', 'age'])
 
 print(df1)
 
-ins = pd.read_csv('insurance_data.csv')
+#ins = pd.read_csv('insurance_data.csv')
 
-df3 = ins[(ins['age']>20) & (ins['smoker'] == 'yes') & (ins['sex'] == 'female')]
-print(df3)
+#df3 = ins[(ins['age']>20) & (ins['smoker'] == 'yes') & (ins['sex'] == 'female')]
+
+df = pd.DataFrame([
+    ['Amy', 'Assignent 1', 75],
+    ['Amy', 'Assignment 2', 35],
+    ['Amy', 'Assignent 3', 80],
+    ['Amy', 'Assignment 4', 55],
+    ['Bob', 'Assignment 1', 99],
+    ['Bob', 'Assignment 2', 35],
+    ['Bob', 'Assignment 3', 50],
+    ['Bob', 'Assignment 4', 70]
+], columns = ['Name', 'Assignment', 'Grade'])
+
+print(df.groupby('Name').Grade.mean())
+print(df.groupby('Name').Grade.std())
+print(df.groupby('Name').Grade.median())
+print(df.groupby('Name').Grade.count())
